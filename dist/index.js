226059ee1611ee1c9a8a3fa66ce5d6ac6d39ebd7
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-01-10 14:10:39 +0600

    6. Parameter Properties Shorthand

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,26 @@
 "use strict";
+// class Player {
+//   public first: string;
+//   public readonly last: string;
+//   private score: number = 0;
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+//   private secretMethod(): void {
+//     console.log("SECRET METHOD");
+//   }
+// }
 class Player {
     constructor(first, last) {
-        this.score = 0;
         this.first = first;
         this.last = last;
+        this.score = 0;
     }
     secretMethod() {
         console.log("SECRET METHOD");
     }
 }
 const elton = new Player("Elton", "Steele");
-elton.first = "elton";
+// elton.first = "elton";
 // elton.secretMethod()
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
+// class Player {
+//   public first: string;
+//   public readonly last: string;
+//   private score: number = 0;
+
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+
+//   private secretMethod(): void {
+//     console.log("SECRET METHOD");
+//   }
+// }
+
 class Player {
-  public first: string;
-  public readonly last: string;
   private score: number = 0;
 
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
-  }
+  constructor(public first: string, public last: string) {}
 
   private secretMethod(): void {
     console.log("SECRET METHOD");
   }
 }
-
 const elton = new Player("Elton", "Steele");
-elton.first = "elton";
+// elton.first = "elton";
 // elton.secretMethod()
