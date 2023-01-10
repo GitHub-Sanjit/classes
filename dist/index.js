d3d04016bb432666909366b044f0dcf7189469ee
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-01-10 14:58:53 +0600

    10. Creating Abstract Classes

--- dist/index.js
+++ dist/index.js
@@ -59,3 +59,35 @@ class Jacket {
 }
 const bike1 = new Bike("red");
 const jacket1 = new Jacket("Prada", "Black");
+class Employee {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+    greet() {
+        console.log("Hello");
+    }
+}
+class FullTimeEmployee extends Employee {
+    constructor(first, last, salary) {
+        super(first, last);
+        this.salary = salary;
+    }
+    getPay() {
+        return this.salary;
+    }
+}
+class PartTimeEmployee extends Employee {
+    constructor(first, last, hourlyRate, hoursWorked) {
+        super(first, last);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+    getPay() {
+        return this.hourlyRate * this.hoursWorked;
+    }
+}
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay());
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay());
